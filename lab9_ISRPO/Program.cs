daa0433336b255e542b21ec3d564caa4ded724cf
Грачева Юлия ИПсп-121 <GrachevaIPsp121@example.com>
2024-11-15 01:00:52 +0300

    Add a console app

---
+++ lab9_ISRPO/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите ваше имя: ");
+        string name = Console.ReadLine();
+        Console.Write("Введите ваш возраст: ");
+        string age = Console.ReadLine();
+        Console.WriteLine($"Привет, {name}! Вам {age} лет.");
+    }
+}
